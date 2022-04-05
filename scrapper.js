e6b65d528fd7cd127d0152832fe4aa87b972a774
Aiden Bi <wenhaobi1999@hotmail.com>
2022-04-04 23:18:51 -0400

    scrapper for tracker

---
+++ scrapper.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const name = "Beifeng"
+const hashtag= "1800"
+
+
+export async function trackerScrapper(name, hashtag) {
+    const url = `https://tracker.gg/valorant/profile/riot/${name}%23${hashtag}/overview`;
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        const ratingEntry = $('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.segment-stats.area-main-stats.card.bordered.header-bordered.responsive > div.highlighted.highlighted--giants > div.highlighted__content > div > div.valorant-highlighted-content__stats > div:nth-child(2) > span.valorant-highlighted-stat__value').text()
+        return ratingEntry
+
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+trackerScrapper(name,hashtag);
+
+
