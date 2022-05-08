dcdb105bd19f8c9176641dd20094088265ab7a2a
Aiden Bi <wenhaobi1999@hotmail.com>
2022-05-07 19:01:28 -0700

    scrapper bug fix

--- scrapper.js
+++ scrapper.js
@@ -5,19 +5,25 @@ async function trackerScrapper(names, hashtag) {
     const data = []
     for (var i = 0; i < names.length; i++) {
         const url = `https://tracker.gg/valorant/profile/riot/${names[i]}%23${hashtag[i]}/overview`;
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        const todayDate = `${yyyy}-${mm}-${dd}`
         try {
             const response = await axios.get(url);
             const $ = cheerio.load(response.data);
-            const ratingEntry = $('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.segment-stats.area-main-stats.card.bordered.header-bordered.responsive > div.highlighted.highlighted--giants > div.highlighted__content > div > div.valorant-highlighted-content__stats > div:nth-child(2) > span.valorant-highlighted-stat__value').text()
-            console.log(ratingEntry)
-            data.push(ratingEntry)
+            const ratingEntry = $('#app > div.trn-wrapper > div.trn-container > div > main > div.content.no-card-margin > div.site-container.trn-grid.trn-grid--vertical.trn-grid--small > div.trn-grid.container > div.area-main > div.segment-stats.area-main-stats.card.bordered.header-bordered.responsive > div.highlighted.highlighted--giants > div.highlighted__content > div > div.valorant-highlighted-content__stats > div:nth-child(2) > span.valorant-highlighted-stat__value').text()
+            data.push({ name: names[i], tag: hashtag[i], date: todayDate, rr: ratingEntry })
         } catch (e) {
             console.error(e);
         }
     }
     console.log(data)
+    return data
 };
 
-trackerScrapper(['Beifeng'], ['1800'])
+trackerScrapper(['Beifeng', 'Hiding Duck'], ['1800', 'ELLOW'])
 
 module.exports = trackerScrapper
